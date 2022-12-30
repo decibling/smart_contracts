32b8df7c4e39f4c6a6d22014b575372aa94d3b58
khanhtsn <khanh@zack.vn>
2022-12-30 20:06:05 +0700

    feat: update smart contract

--- contracts/DbAudio2.sol
+++ contracts/DbAudio2.sol
@@ -6,73 +6,65 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract DbAudio is ERC721, Ownable, ERC721URIStorage {
+contract DbAudio is ERC721, Ownable, ERC721URIStorage, ReentrancyGuard {
+    using SafeMath for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    IERC20 private froyContract;
+    IERC20 private token;
 
     /// @notice global platform fees, assumed to always be to 1 decimal place i.e. 25 = 2.5%
     uint256 private firstSaleFee = 125;
     uint256 private secondSaleFee = 100;
 
     /// @notice where to send platform fee funds to
-    address payable public platformFeeRecipient;
-
-    enum BidStatus {
-        NOTREADY,
-        START,
-        BIDING,
-        END
-    }
+    address payable private platformFeeRecipient;
 
     enum AudioStatus {
         NEW,
-        BIDING,
-        SELLING,
+        BIDDING,
         OWNED
     }
 
-    struct Bid {
+    struct TopBid {
         address user;
         uint256 price;
         uint256 timestamp;
     }
 
-    struct Bidding {
+    struct Auction {
         address winner;
-        uint256 price;
-        BidStatus status;
+        uint256 startPrice;
+        uint256 increment;
         uint256 startTime;
         uint256 endTime;
-        uint256 currentSession;
-        mapping(uint256 => Bid) biddingSessions;
+        bool resulted;
     }
 
     struct AudioInfo {
         address owner;
-        string url;
         string name;
         uint256 price;
-        AudioStatus status;
-        mapping(uint256 => Bidding) biddingList;
-        uint256 currentBidding;
         uint256 saleCount;
+        AudioStatus status;
     }
 
     mapping(string => AudioInfo) public listNFT;
     mapping(string => uint256) public tokenIdMapping;
-    mapping(address => uint256[]) public listOwnToken;
-    mapping(uint256 => uint256) public  indexOfToken;
+    mapping(uint256 => Auction) public auctions;
+    mapping(uint256 => TopBid) public topBids;
 
     event UpdatePlatformFees(uint256 firstSaleFee, uint256 secondSaleFee);
     event UpdatePlatformFeeRecipient(address payable platformFeeRecipient);
     event CreateNFT(string uri, uint256 index);
-    event BidEvent(string uri, uint256 startPrice, uint256 startTime, uint256 endTime);
-    event SettleBidding(string uri, address oldowner, address newowner, uint256 totalPrice, uint256 platformValue, uint256 saleCount);
+    event CreateBid(string uri, uint256 startPrice, uint256 increment, uint256 startTime, uint256 endTime);
+    event BidPlaced(string uri, address user, uint256 amount);
+    event SettleBid(string uri, address oldowner, address newowner, uint256 totalPrice, uint256 platformValue, uint256 saleCount);
 
     constructor() ERC721("Decibling", "DB") {
-        froyContract = IERC20(0xAa8ADb51329BA9640D86Aa10b0F374d97A7B31d9);
+        token = IERC20(0xd9145CCE52D386f254917e481eB44e9943F39138);
         platformFeeRecipient = payable(msg.sender);
     }
 
@@ -92,174 +84,163 @@ contract DbAudio is ERC721, Ownable, ERC721URIStorage {
         return super.tokenURI(tokenId);
     }
 
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory _data
-    ) public override {
-        listOwnToken[to].push(tokenId);
-        listOwnToken[from][indexOfToken[tokenId]] = listOwnToken[from][
-            listOwnToken[from].length - 1
-        ];
-        listOwnToken[from].pop();
-        indexOfToken[tokenId] = listOwnToken[to].length - 1;
-        return super.safeTransferFrom(from, to, tokenId, _data);
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual override {
-        listOwnToken[to].push(tokenId);
-        listOwnToken[from][indexOfToken[tokenId]] = listOwnToken[from][
-            listOwnToken[from].length - 1
-        ];
-        listOwnToken[from].pop();
-        indexOfToken[tokenId] = listOwnToken[to].length - 1;
-        super.transferFrom(from, to, tokenId);
-    }
-
     /*
         createNFT
         uri : string of resource
         name : internal id
     */
     function createNFT(
-        string memory uri,
-        string memory name
-    ) public returns (uint256) {
-        AudioInfo storage currentNFT = listNFT[uri];
-        require(keccak256(bytes(currentNFT.url)) != keccak256(bytes(uri)), "2");
+        string calldata uri,
+        string calldata name
+    ) public {
+        require(tokenIdMapping[uri] == 0, "2");
         _tokenIds.increment();
-        listNFT[uri].owner = msg.sender;
-        listNFT[uri].url = uri;
-        listNFT[uri].name = name;
-        listNFT[uri].status = AudioStatus.NEW;
-        listNFT[uri].saleCount = 0;
-        listNFT[uri].currentBidding = 0;
         uint256 newItemId = _tokenIds.current();
-        _mint(msg.sender, newItemId);
+        _safeMint(msg.sender, newItemId);
         _setTokenURI(newItemId, uri);
         tokenIdMapping[uri] = newItemId;
-        listOwnToken[msg.sender].push(newItemId);
-        indexOfToken[newItemId] = listOwnToken[msg.sender].length - 1;
+
+        listNFT[uri] = AudioInfo({
+            owner: _msgSender(),
+            name: name,
+            status: AudioStatus.NEW,
+            price: 0,
+            saleCount: 0
+        });
+
         emit CreateNFT(uri, newItemId);
-        return newItemId;
     }
 
+    /**
+     @notice Method for create a bid
+     @param uri string the platform token uri
+     @param startPrice uint256 the start price
+     @param increment uint256 price increment
+     @param startTime uint256 the start time
+     @param endTime uint256 the end time
+     */
     function createBidding(
         string memory uri,
         uint256 startPrice,
+        uint256 increment,
         uint256 startTime,
         uint256 endTime
     ) public payable {
+        AudioInfo storage currentNFT = listNFT[uri];
+        uint256 itemId = tokenIdMapping[uri];
+
         require(startTime < endTime, "18");
-        AudioInfo storage currentNFT = listNFT[uri];
-        require(keccak256(bytes(currentNFT.url)) == keccak256(bytes(uri)), "4");
-        require(currentNFT.owner == msg.sender, "6");
+        require(tokenIdMapping[uri] > 0, "2");
+        require(currentNFT.owner == _msgSender(), "6");
         require(endTime > _getNow(), "7");
-        require(
-            currentNFT.status == AudioStatus.NEW ||
-                currentNFT.status == AudioStatus.OWNED,
-            "8"
-        );
+        require(currentNFT.status == AudioStatus.NEW || currentNFT.status == AudioStatus.OWNED, "8");
+        require(increment > 0, "22");
+        require(endTime >= startTime + 300, "24"); //end time must be after at least 5 mins
 
-        currentNFT.currentBidding += 1;
-        currentNFT.biddingList[currentNFT.currentBidding].winner = address(0);
-        currentNFT.biddingList[currentNFT.currentBidding].price = startPrice;
-        currentNFT.biddingList[currentNFT.currentBidding].status = startTime >
-            _getNow()
-            ? BidStatus.START
-            : BidStatus.NOTREADY;
-        currentNFT.biddingList[currentNFT.currentBidding].startTime = startTime;
-        currentNFT.biddingList[currentNFT.currentBidding].endTime = endTime;
-        currentNFT.biddingList[currentNFT.currentBidding].currentSession = 0;
-        currentNFT.status = AudioStatus.BIDING;
-        emit BidEvent(uri, startPrice, startTime, endTime);
+        auctions[itemId] = Auction({
+            winner: address(0),
+            startPrice: startPrice,
+            increment: increment,
+            startTime: startTime,
+            endTime: endTime,
+            resulted: false
+        });
+
+        currentNFT.status = AudioStatus.BIDDING;
+
+        emit CreateBid(uri, startPrice, increment, startTime, endTime);
     }
 
-    function bid(string memory uri, uint256 _amount) public payable {
-        froyContract.transferFrom(msg.sender, address(this), _amount);
+    /**
+     @notice Method for place a bid
+     @param uri string the platform token uri
+     @param _amount uint256 the amount to bid
+     */
+    function bid(string memory uri, uint256 _amount) public payable nonReentrant {
         AudioInfo storage currentNFT = listNFT[uri];
-        require(keccak256(bytes(currentNFT.url)) == keccak256(bytes(uri)), "4");
-        require(currentNFT.status == AudioStatus.BIDING, "10");
-        require(currentNFT.currentBidding != 0, "11");
-        Bidding storage biddingOfNFT = currentNFT.biddingList[
-            currentNFT.currentBidding
-        ];
-        require(
-            biddingOfNFT.startTime <= _getNow() &&
-                _getNow() <= biddingOfNFT.endTime,
-            "12"
-        );
-        address lastWinner = biddingOfNFT.winner;
-        uint256 lastPrice = biddingOfNFT.price;
-        if (lastWinner == address(0)) {
-            require(biddingOfNFT.price <= _amount, "13");
+        uint256 itemId = tokenIdMapping[uri];
+
+        require(tokenIdMapping[uri] > 0, "2");
+        require(currentNFT.status == AudioStatus.BIDDING, "8");
+        
+        Auction storage auction = auctions[itemId];
+        require(auction.startTime <= _getNow() && _getNow() <= auction.endTime, "12");
+        require(token.transferFrom(msg.sender, address(this), _amount), "26");
+
+        TopBid storage topBid = topBids[itemId];
+        address topBidUser = topBid.user;
+        uint256 lastPrice = topBid.price;
+        if (topBidUser == address(0)) {
+            if (_amount < lastPrice) revert ("13");
         } else {
-            require(biddingOfNFT.price < _amount, "13");
+            if (_amount < lastPrice + auction.increment) revert ("13");
+            require(token.transfer(topBidUser, lastPrice), "26");
         }
-        biddingOfNFT.status = BidStatus.BIDING;
-        Bid memory bidv = Bid(msg.sender, _amount, _getNow());
-        biddingOfNFT.currentSession += 1;
-        biddingOfNFT.biddingSessions[biddingOfNFT.currentSession] = bidv;
-        // set winner and larger
-        biddingOfNFT.winner = msg.sender;
-        biddingOfNFT.price = _amount;
-        if(lastWinner != address(0)){
-            froyContract.transfer(lastWinner, lastPrice);
-        }
+
+        // assign top bidder and bid time
+        topBid.user = _msgSender();
+        topBid.price = _amount;
+        topBid.timestamp = _getNow();
+
+        emit BidPlaced(uri, _msgSender(), _amount);
     }
 
-    function settleBiddingSession(string memory uri) public payable {
+    /**
+     @notice Method for settle a bid, or cancel if no winner
+     @param uri string the platform token uri
+     */
+    function settleBiddingSession(string memory uri) public payable nonReentrant {
         AudioInfo storage currentNFT = listNFT[uri];
-        require(keccak256(bytes(currentNFT.url)) == keccak256(bytes(uri)), "4");
-        require(currentNFT.status == AudioStatus.BIDING, "14");
-        require(currentNFT.currentBidding != 0, "11");
+        uint256 itemId = tokenIdMapping[uri];
+        
+        require(currentNFT.owner == _msgSender() || owner() == _msgSender(), "6");
+        require(tokenIdMapping[uri] > 0, "2");
+        require(currentNFT.status == AudioStatus.BIDDING, "8");
         require(platformFeeRecipient != address(0), "19");
-        Bidding storage biddingOfNFT = currentNFT.biddingList[
-            currentNFT.currentBidding
-        ];
-        require(
-            biddingOfNFT.startTime < _getNow() &&
-                _getNow() > biddingOfNFT.endTime,
-            "15"
-        );
-        require(
-            biddingOfNFT.status == BidStatus.BIDING ||
-                currentNFT.status == AudioStatus.BIDING,
-            "16"
-        );
-        uint256 totalPrice = biddingOfNFT.price;
+
+        Auction storage auction = auctions[itemId];
+        // Ensure auction not already resulted
+        require(!auction.resulted, "27");
+        require(auction.startTime < _getNow() && _getNow() > auction.endTime, "15");
+
+        // Get info on who the highest bidder is
+        TopBid storage topBid = topBids[itemId];
+        address winner = topBid.user;
+
+        // Result the auction
+        auction.resulted = true;
+
+        // Clean up the highest bid
+        delete topBids[itemId];
+
+        uint256 totalPrice = topBid.price;
         uint256 platformValue;
         address oldOwner = currentNFT.owner;
-        if (biddingOfNFT.winner != address(0)) {
+        if (winner != address(0)) {
             if (currentNFT.saleCount == 0 && firstSaleFee > 0) {
-                platformValue = totalPrice * firstSaleFee / 1e3;
+                platformValue = totalPrice.mul(firstSaleFee).div(1e3);
             } else {
-                platformValue = totalPrice * secondSaleFee / 1e3;
+                platformValue = totalPrice.mul(secondSaleFee).div(1e3);
             }
-
-            require(platformValue > 0, "20");
-            safeTransferFrom(
-                currentNFT.owner,
-                biddingOfNFT.winner,
-                tokenIdMapping[uri]
-            );
-            currentNFT.owner = biddingOfNFT.winner;
-            currentNFT.price = totalPrice;
-            //to seller
-            froyContract.transfer(currentNFT.owner, totalPrice-platformValue);
+            if (platformValue > 0) revert ("20");
+            //to seller - old owner
+            require(token.transfer(oldOwner, totalPrice-platformValue), "26");
             //to platform
-            froyContract.transfer(platformFeeRecipient, platformValue);
-        }
-        biddingOfNFT.status = BidStatus.END;
-        currentNFT.status = AudioStatus.OWNED;
-        currentNFT.saleCount += 1;
+            require(token.transfer(platformFeeRecipient, platformValue), "26");
+            //to new owner
+            safeTransferFrom(oldOwner, winner, tokenIdMapping[uri]);
+            currentNFT.owner = winner;
+            currentNFT.price = totalPrice;
+            currentNFT.status = AudioStatus.OWNED;
+            currentNFT.saleCount = currentNFT.saleCount.add(1);
 
-        emit SettleBidding(uri, oldOwner, biddingOfNFT.winner, totalPrice, platformValue, currentNFT.saleCount);
+            auction.winner = winner;
+
+            emit SettleBid(uri, oldOwner, auction.winner, totalPrice, platformValue, currentNFT.saleCount);
+        } else {
+            currentNFT.status = AudioStatus.NEW;
+        }
+
     }
 
     function _getNow() internal view virtual returns (uint256) {
