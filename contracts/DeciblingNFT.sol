790981cc41a61508b804842e572b143a82c38af7
possc <34095856+possc@users.noreply.github.com>
2023-03-31 18:26:28 +0700

    Update merkle tree testing

--- contracts/DeciblingNFT.sol
+++ contracts/DeciblingNFT.sol
@@ -71,7 +71,8 @@ contract DeciblingNFT is
     ) external {
         // Validate merkle proof || skip if Merkle Root = 0
         if (_merkleRoot != bytes32(0)) {
-            bytes32 merkleLeaf = keccak256(bytes.concat(keccak256(abi.encode(_msgSender()))));
+            // bytes32 merkleLeaf = keccak256(bytes.concat(keccak256(abi.encode(_msgSender()))));
+            bytes32 merkleLeaf = keccak256(abi.encodePacked(_msgSender()));
             require(
                 MerkleProofUpgradeable.verify(proof, _merkleRoot, merkleLeaf),
                 "Invalid proof"
--- test/DeciblingNFT.test.js
+++ test/DeciblingNFT.test.js
@@ -5,39 +5,41 @@ const {
 const {
     expect
 } = require("chai");
-const {
-    StandardMerkleTree
-} = require("@openzeppelin/merkle-tree");
+// const {
+//     StandardMerkleTree
+// } = require("@openzeppelin/merkle-tree");
 const fs = require('fs');
+const { keccak256 } = require("ethers/lib/utils");
+const { createMerkleTree, getMerkleProof } = require("./helper/genMerkle");
 
-function buildTree(values) {
-    // (1)
-    // (2)
-    const tree = StandardMerkleTree.of(values, ["address"]);
-    // (3)
-    console.log('Merkle Root:', tree.root);
-    // (4)
-    fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
+// function buildTree(values) {
+//     // (1)
+//     // (2)
+//     const tree = StandardMerkleTree.of(values, ["address"]);
+//     // (3)
+//     console.log('Merkle Root:', tree.root);
+//     // (4)
+//     fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
 
-    return tree.root;
-}
+//     return tree.root;
+// }
 
-function obtainProof(addr) {
-    // (1)
-    const tree = StandardMerkleTree.load(JSON.parse(fs.readFileSync("tree.json")));
+// function obtainProof(addr) {
+//     // (1)
+//     const tree = StandardMerkleTree.load(JSON.parse(fs.readFileSync("tree.json")));
 
-    // (2)
-    for (const [i, v] of tree.entries()) {
-        if (v[0] === addr) {
-            // (3)
-            const proof = tree.getProof(i);
-            console.log('Value:', v);
-            console.log('Proof:', proof);
+//     // (2)
+//     for (const [i, v] of tree.entries()) {
+//         if (v[0] === addr) {
+//             // (3)
+//             const proof = tree.getProof(i);
+//             console.log('Value:', v);
+//             console.log('Proof:', proof);
 
-            return proof;
-        }
-    }
-}
+//             return proof;
+//         }
+//     }
+// }
 
 describe("DeciblingNFT", function () {
     let DeciblingNFT, deciblingNFT, owner, addr1, addr2, addr3, addr4;
@@ -79,25 +81,70 @@ describe("DeciblingNFT", function () {
         });
     });
 
-    describe("Minting from artists", function () {
-        it("Should fail mint NFT if not on the list", async function () {
-            let root = buildTree([
-                [owner.address],
-                [addr1.address],
-            ]);
+    // describe("Minting from artists", function () {
+    //     it("Should fail mint NFT if not on the list", async function () {
+    //         let root = buildTree([
+    //             [owner.address],
+    //             [addr1.address],
+    //         ]);
 
-            expect(await deciblingNFT.connect(owner).setMerkleRoot(root))
+    //         expect(await deciblingNFT.connect(owner).setMerkleRoot(root))
 
-            const name = "test";
-            const uri = "https://example.com/testaudio";
-            expect(await deciblingNFT.connect(addr1).mint(obtainProof(addr1.address), name, uri));
-        });
+    //         const name = "test";
+    //         const uri = "https://example.com/testaudio";
+    //         expect(await deciblingNFT.connect(addr1).mint(obtainProof(addr1.address), name, uri));
+    //     });
+    // });
+
+    // describe("Upgrading", function () {
+    //     it("Should be able to upgrade the contract", async function () {
+    //         const newImplementation = await ethers.getContractFactory("DeciblingNFT");
+    //         await upgrades.prepareUpgrade(deciblingNFT.address, newImplementation);
+    //     });
+    // });
+});
+
+describe.only("DeciblingNFT with Merkle Proof", function () {
+    let DeciblingNFT, deciblingNFT, owner, addr1, addr2;
+    let merkleTree, merkleRoot, validProof, invalidProof;
+
+    beforeEach(async () => {
+        DeciblingNFT = await ethers.getContractFactory("DeciblingNFT");
+        [owner, addr1, addr2] = await ethers.getSigners();
+        deciblingNFT = await upgrades.deployProxy(DeciblingNFT, { initializer: "initialize" });
+
+        // Generate Merkle Tree, Root, and Proofs
+        const elements = [addr1.address, addr2.address];
+        const { merkleTree: tree, merkleRoot: root } = createMerkleTree(elements);
+        merkleTree = tree;
+        merkleRoot = root;
+        validProof = getMerkleProof(merkleTree, addr1.address);
+        invalidProof = getMerkleProof(merkleTree, owner.address);
+
+        // Set the Merkle Root
+        await deciblingNFT.connect(owner).setMerkleRoot(merkleRoot);
     });
 
-    describe("Upgrading", function () {
-        it("Should be able to upgrade the contract", async function () {
-            const newImplementation = await ethers.getContractFactory("DeciblingNFT");
-            await upgrades.prepareUpgrade(deciblingNFT.address, newImplementation);
-        });
+    it("Mint with valid proof", async () => {
+        const name = "Valid Audio";
+        const uri = "https://ipfs.io/ipfs/VALID_AUDIO";
+
+        await expect(deciblingNFT.connect(addr1).mint(validProof, name, uri))
+            .to.emit(deciblingNFT, "Minted")
+            .withArgs(name, 0);
+
+        const audioInfo = await deciblingNFT.audioInfos(0);
+        console.log("🚀 ~ file: DeciblingNFT.test.js:137 ~ it ~ audioInfo:", audioInfo)
+        expect(audioInfo).to.equal(name);
+
+        const tokenURI = await deciblingNFT.tokenURI(0);
+        expect(tokenURI).to.equal(uri);
+    });
+
+    it("Mint with invalid proof", async () => {
+        const name = "Invalid Audio";
+        const uri = "https://ipfs.io/ipfs/INVALID_AUDIO";
+
+        await expect(deciblingNFT.connect(addr1).mint(invalidProof, name, uri)).to.be.revertedWith("Invalid proof");
     });
 });
