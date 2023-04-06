0ab33099537b5e9a5860388141b4ce750372d15f
khanhtsn <khanh@zack.vn>
2023-04-06 16:51:06 +0700

    feat: finalize token deployment

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FroggilyToken is ERC20, Ownable {
+contract FroggilyToken is ERC20 {
     constructor() ERC20("Froggily", "FROY") {
-        _mint(msg.sender, 100000000000000000000000000000);
+        _mint(msg.sender, 100_000_000_000 * 10 ** decimals());
     }
 }
