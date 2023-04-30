e03c118efb96c37ba05b01172b789c9bb96d2fc6
KhanhTSN <khanhtsn@gmail.com>
2023-05-01 05:46:46 +0700

    fix: update staking deploy

--- scripts/deployStaking.js
+++ scripts/deployStaking.js
@@ -1,23 +1,38 @@
-const { ethers, upgrades } = require("hardhat");
+const {
+    ethers,
+    upgrades
+} = require("hardhat");
 
 async function main() {
     const [deployer] = await ethers.getSigners();
 
     // Deploy MyToken contract
-    // const MyToken = await ethers.getContractFactory("MyToken");
-    // const myToken = await MyToken.deploy();
-    // await myToken.deployed();
-    // console.log("MyToken deployed to:", myToken.address); 0x5FbDB2315678afecb367f032d93F642f64180aa3
+    const MyToken = await ethers.getContractFactory("FroggilyToken");
+    const myToken = await MyToken.deploy();
+    await myToken.deployed();
+    console.log("FroggilyToken deployed to:", myToken.address);
 
     // Deploy DeciblingStaking contract
     const DeciblingStaking = await ethers.getContractFactory("DeciblingStaking");
     const dbStaking = await upgrades.deployProxy(
         DeciblingStaking,
-        [ "0x5FbDB2315678afecb367f032d93F642f64180aa3", deployer.address],
-        { initializer: "initialize" }
+        [myToken.address], {
+            initializer: "initialize"
+        }
     );
     await dbStaking.deployed();
     console.log("DeciblingStaking deployed to:", dbStaking.address);
+
+    // Deploy DeciblingReserve contract
+    const DeciblingReserve = await ethers.getContractFactory("DeciblingReserve");
+    const dbReserve = await upgrades.deployProxy(
+        DeciblingReserve,
+        [myToken.address], {
+            initializer: "initialize"
+        }
+    );
+    await dbReserve.deployed();
+    console.log("DeciblingReserve deployed to:", dbReserve.address);
 }
 
 main()
